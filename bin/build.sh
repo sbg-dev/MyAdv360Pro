3c0cf7aa513f178314c70551c885087953dad757
Sebastian Gnip <20845980+sbg-dev@users.noreply.github.com>
2025-03-18 12:45:39 +0100

    Add logging for directory contents in build script

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,14 @@ PWD=$(pwd)
 TIMESTAMP="${TIMESTAMP:-$(date -u +"%Y%m%d%H%M")}"
 COMMIT="${COMMIT:-$(echo xxxxxx)}"
 
+ls -la ${PWD}
 # West Build (left)
+ls -la ${PWD}/config
+
+west init -l config
+west update
+west zephyr-export
+
 west build -s zmk/app -p -d build/left -b adv360_left -S studio-rpc-usb-uart -- -DZMK_CONFIG="${PWD}/config" -DCONFIG_ZMK_STUDIO=y
 # Adv360 Left Kconfig file
 grep -vE '(^#|^$)' build/left/zephyr/.config
@@ -15,6 +22,7 @@ cp build/left/zephyr/zmk.uf2 "./firmware/${TIMESTAMP}-${COMMIT}-left-clique.uf2"
 
 # Build right side if selected
 if [ "${BUILD_RIGHT}" = true ]; then
+    ls -la ${PWD}/config
     # West Build (right)
     west build -s zmk/app -p -d build/right -b adv360_right -- -DZMK_CONFIG="${PWD}/config"
     # Adv360 Right Kconfig file
